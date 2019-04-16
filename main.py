6d3fbc45fc734ca7f2250ee94e234dea52969c29
JoaoP <contato.joaocomp@gmail.com>
2019-04-16 18:13:17 -0300

    Wrapper do Kinect implementado

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
 	frame_count, frame_limit = 0, args.time_limit * frame_rate
 	while frame_count < frame_limit:
 		(success, frame) = wrapper.get_frame()
-		if not success or wrapper.stop_condition():
+		if not success or cv2.waitKey(1) & 0xFF == ord('q'):
 			break
 		elif frames_to_skip > 0:
 			frames_to_skip -= 1
@@ -24,10 +24,10 @@ if __name__ == "__main__":
 			if face_rect is not None:
 				landmarks = predictor.detect_landmarks(image = frame, rect = face_rect)
 				frame = extractor.extract_roi(frame, landmarks)
-				
+
 				strategy.process(frame)
-				cv2.imshow("Frame", frame)
-				
+
+				cv2.imshow("rPPG Tracker", frame)
 				frame_count += 1
 			else:
 				frames_to_skip = args.skip_count
