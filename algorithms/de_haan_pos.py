c0055ca643da07a1942056d3bb2b68f8c4519d41
JoaoP <contato.joaocomp@gmail.com>
2019-04-29 15:13:58 -0300

    Inserindo visualização / Print após processamento

--- algorithms/de_haan_pos.py
+++ algorithms/de_haan_pos.py
@@ -39,29 +39,38 @@ class DeHaanPOS(DefaultStrategy):
 				C_mean_diag_inv = np.linalg.inv(C_mean_diag)
 				
 				Cn = np.matmul(C_mean_diag_inv, C)
-				S = np.matmul(np.array([[0, 1, -1], [-2, 1, 1]]), Cn)
-				P = np.matmul(np.array([1, np.std(S[0]) / np.std(S[1])]), S)
 
-				signal[f_id : f_id + window_size] += (P - np.mean(P))
+				S = np.matmul(np.array([[0, 1, -1], [-2, 1, 1]]), Cn)
+				P = S[0] + (np.std(S[0]) / np.std(S[1])) * S[1]
+
+				signal[f_id : f_id + window_size] += P - np.mean(P)
 
 		return (signal - np.mean(signal)) / np.std(signal)
 
 	def show_results(self, frame_rate = 30, window_size = 60, plot = True):
-		def get_order(size):
-			return (size - 6) // 3
-
 		signal = self.measure_reference(frame_rate = frame_rate, window_size = window_size)
+		filtered_signal = self.bandpass_filter(signal, frame_rate = frame_rate, min_freq = 0.6, max_freq = 4.0, order = 128)
+
 		x, y = self.get_fft(signal, frame_rate = frame_rate)
+		x_f, y_f = self.get_fft(filtered_signal, frame_rate = frame_rate)
 
 		if plot == True:
-			plt.subplot(2, 1, 1)
-			plt.plot(signal)
+			plt.subplot(3, 1, 1)
+			plt.plot(self.temporal_means[:, 0], 'r')
+			plt.plot(self.temporal_means[:, 1], 'g')
+			plt.plot(self.temporal_means[:, 2], 'b')
 
-			plt.subplot(2, 1, 2)
+			plt.subplot(3, 1, 2)
+			plt.plot(signal)
+			plt.plot(filtered_signal)
+
+			plt.subplot(3, 1, 3)
 			plt.plot(x, y)
+			plt.plot(x_f, y_f)
 
 			plt.show()
 
-		print("[DeHaanPOS] {0:2f} bpm.".format(60.0 * x[np.argmax(y)]))
+		print("[DeHaanPOS - Without BPF] {0:2f} bpm.".format(60.0 * x[np.argmax(y)]))
+		print("[DeHaanPOS - With BPF] {0:2f} bpm.".format(60.0 * x_f[np.argmax(y_f)]))
 
 		plt.show()
