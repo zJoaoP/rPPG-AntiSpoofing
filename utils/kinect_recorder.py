798240481a4fc7dc79dd08f004b8d2803545a75b
JoaoP <contato.joaocomp@gmail.com>
2019-05-02 16:21:25 -0300

    Corrigindo falhas de segmentação durante a gravação de vídeos

--- utils/kinect_recorder.py
+++ utils/kinect_recorder.py
@@ -1,14 +1,15 @@
 from wrappers import Kinect_Wrapper
 
 import cv2
+import sys
 
 if __name__ == "__main__":
-	stream = Kinect_Wrapper(width = 1920, height = 1080)
+	stream = Kinect_Wrapper(width = 1280, height = 720)
 	frame_count, frame_rate = 0, stream.get_frame_rate()
 	record_time = 30
 
 	fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-	output = cv2.VideoWriter('output.avi',fourcc, 30.0, (1920, 1080))
+	output = cv2.VideoWriter('output.avi',fourcc, 30.0, (1280, 720))
 
 	while frame_count < frame_rate * record_time:
 		(success, frame) = stream.get_frame()
@@ -16,11 +17,13 @@ if __name__ == "__main__":
 		if not success or cv2.waitKey(1) & 0xFF == ord('q'):
 			break
 
-		cv2.imshow("Kinect Recorder", frame[:, :, :3])
+		cv2.imshow("Kinect Recorder", frame)
 		output.write(frame)
+
 		print(frame.shape)
 
 		frame_count += 1
 
 	output.release()
 	cv2.destroyAllWindows()
+	sys.exit(0)
--- utils/wrappers.py
+++ utils/wrappers.py
@@ -12,11 +12,8 @@ class Wrapper:
 	def get_frame_rate(self):
 		return 0
 
-	def stop_condition(self):
-		return True
-
 class Kinect_Wrapper(Wrapper):
-	def __init__(self, width = 1280, height = 720):
+	def __init__(self, width = 1920, height = 1080):
 		super(Kinect_Wrapper, self).__init__(width, height)
 
 		from pylibfreenect2 import Freenect2, SyncMultiFrameListener, Registration
@@ -24,24 +21,27 @@ class Kinect_Wrapper(Wrapper):
 
 		self.pipeline = OpenGLPacketPipeline()
 		self.freenect = Freenect2()
+
+		self.num_devices = self.freenect.enumerateDevices()
+		assert self.num_devices > 0, "[Kinect Wrapper] Nenhum dispositivo Kinect conectado!"
 		
 		self.serial = self.freenect.getDeviceSerialNumber(0)
 		self.device = self.freenect.openDevice(self.serial, pipeline = self.pipeline)
 		self.listener = SyncMultiFrameListener(FrameType.Color)
 		
 		self.device.setColorFrameListener(self.listener)
-		self.device.start()
+		self.device.startStreams(rgb = True, depth = False)
 
 		self.registration = Registration(self.device.getIrCameraParams(), self.device.getColorCameraParams())
 
 	def __del__(self):
-		self.device.stop()
-		self.device.close()
+		if hasattr(self, "device") and self.device is not None:
+			self.device.stop()
+			self.device.close()
 
 	def get_frame(self):
 		frames = self.listener.waitForNewFrame()
 		color_frame = cv2.resize(frames["color"].asarray(np.uint8), (self.width, self.height), cv2.INTER_CUBIC)
-		color_frame = frames["color"].asarray(np.uint8)
 		self.listener.release(frames)
 		return True, color_frame[:, :, 0 : 3]
 
