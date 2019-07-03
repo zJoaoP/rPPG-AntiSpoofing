431a1a9080e6748076d96631d4d95271f656327b
JoaoP <contato.joaocomp@gmail.com>
2019-07-03 17:58:04 -0300

    Carregamento funcional. Adaptar para SiW

--- data_loader.py
+++ data_loader.py
@@ -53,36 +53,48 @@ class AsyncVideoLoader:
 	def __exit__(self, exec_type, exc_value, traceback):
 		self.capture.release()
 
+
 class AnnotatedVideoLoader:
 	@staticmethod
 	def load_features(source, annotation):
+		print("[DataLoader] Extracting features from '{0}'.".format(source))
+
 		def load_annotations(annotation_location):
-			pass
+			with open(annotation_location) as file:
+				annotations = []
+				for line in file:
+					annotations += [[int(i) for i in line.split(' ')[1:]]]
+
+			return np.array(annotations)
 
 		loader = AsyncVideoLoader(source=source).start()
 		landmark_predictor = LandmarkPredictor()
 		extractor = CheeksAndNose()
 
-		annotations = load_annotations(annotation_folder)
+		annotations = load_annotations(annotation)
 
 		features = np.empty([loader.lenght(), 3], dtype=np.float32)
+		print(loader.lenght())
 		for i in range(loader.lenght()):
 			success, frame = loader.read()
 			cv2.waitKey(1) & 0xFF
 
-			# annotations here.
-			frame = frame[250:500, 250:500, :]
+			if np.sum(annotations[i]) == 0:
+				features[i] = features[i - 1]
+			else:
+				x, y, w, h = annotations[i]
+				frame = frame[y:y+h, x:x+w]
 
-			landmarks = landmark_predictor.detect_landmarks(frame)
+				landmarks = landmark_predictor.detect_landmarks(frame)
 
-			frame = extractor.extract_roi(frame, landmarks).astype(np.float32)
+				frame = extractor.extract_roi(frame, landmarks).astype(np.float32)
 
-			# cv2.imshow("CutFrame", frame.astype(np.uint8))
-
-			frame[frame < 1.0] = np.nan
-			features[i] = np.nanmean(frame, axis=(0, 1))
+				frame[frame < 1.0] = np.nan
+				features[i] = np.nanmean(frame, axis=(0, 1))
 
 		loader.stop()
+		
+		print("[DataLoader] Features extracted from '{0}'.".format(source))
 		return features
 
 class GenericDatasetLoader:
@@ -90,11 +102,10 @@ class GenericDatasetLoader:
 	def walk_and_load_from(root_folder, annotations_folder):
 		def is_video(filename):
 			return 	filename.endswith(".mp4")\
-					or filename.endswith(".avi")
+					or filename.endswith(".mov")
 
 		def is_annotation(filename):
-			return 	filename.endswith(".mp4")\
-					or filename.endswith(".avi")			
+			return 	filename.endswith(".face")
 
 		video_locations = list()
 		for root, dirs, files in os.walk(root_folder):
@@ -105,24 +116,74 @@ class GenericDatasetLoader:
 		annotations = list()
 		for root, dirs, files in os.walk(annotations_folder):
 			for file in files:
-				if is_video(file):
+				if is_annotation(file):
 					annotations.append("{0}/{1}".format(root, file))
 
-		for i in range(len(video_locations)):
+		folder_features = None
+		k = 0
+		for video, annotation in zip(video_locations, annotations):
 			video_data = AnnotatedVideoLoader.load_features(
-							source=video_locations[i],
-							annotation=annotations[i])
-			
-			print(video_data.shape)
+							source=video,
+							annotation=annotation)
+
+			if folder_features is None:
+				folder_features = np.empty([len(video_locations),
+											video_data.shape[0],
+											3])
+			else:
+				if video_data.shape[0] != folder_features.shape[1]:
+					new_video_data = np.zeros(folder_features.shape[1:])
+					new_video_data[:video_data.shape[0]] = video_data
+					video_data = new_video_data.copy()
+
+				folder_features[k] = video_data
+
+			k += 1
 		
-		print(sorted(video_locations))
-		print(sorted(annotations))
+		return folder_features
+
+
+class ReplayAttackLoader:
+	@staticmethod
+	def __load_by_split(source, split):
+		split_attack = GenericDatasetLoader.walk_and_load_from(
+			"{0}/{1}/attack".format(source, split),
+			"{0}/face-locations/{1}/attack".format(source, split)
+		)
+
+		split_attack = GenericDatasetLoader.walk_and_load_from(
+			"{0}/{1}/read".format(source, split),
+			"{0}/face-locations/{1}/read".format(source, split)
+		)
+
+		return split_attack, split_real
+
+	@staticmethod
+	def load_train(source):
+		return ReplayAttackLoader.__load_by_split(source, 'train')
+
+	@staticmethod
+	def load_devel(source):
+		return ReplayAttackLoader.__load_by_split(source, 'devel')
+
+	@staticmethod
+	def load_test(source):
+		return ReplayAttackLoader.__load_by_split(source, 'test')
 
 if __name__ == "__main__":
-	GenericDatasetLoader.walk_and_load_from(
-		"./videos/ReplayAttack/train/fake",
-		"./videos/ReplayAttack/annotations/train/fake"
-	)
+	train_fake, train_real = ReplayAttackLoader.load_train("./videos/ReplayAttack")
+	devel_fake, devel_real = ReplayAttackLoader.load_train("./videos/ReplayAttack")
+	test_fake, test_real = ReplayAttackLoader.load_train("./videos/ReplayAttack")
+
+	np.save("./train_fake.npy", train_fake)
+	np.save("./train_real.npy", train_real)
+
+	np.save("./test_fake.npy", test_fake)
+	np.save("./test_real.npy", test_real)
+	
+	np.save("./devel_fake.npy", devel_fake)
+	np.save("./devel_real.npy", devel_real)
+
 
 	# t0 = time.time()
 	# features = AnnotatedVideoLoader.load_features("./videos/motion.avi", None)
