1263787aeaadfcb1324101b9063b762e728939f0
JoaoP <contato.joaocomp@gmail.com>
2019-08-16 12:41:02 -0300

    General data loading functions

--- utils/loaders.py
+++ utils/loaders.py
@@ -1,195 +1,248 @@
 from utils.landmarks import LandmarkPredictor
 from utils.extractor import CheeksAndNose
-from keras.utils import to_categorical
-
 import numpy as np
+import threading
+import dlib
 import cv2
 import os
 
-class VideoLoader:
-	def __init__(self, time = 10, roi_extractor = CheeksAndNose()):
-		self.roi_extractor = roi_extractor
-		self.time = time
 
-	def is_valid_video(self, filename):
-		valid_extensions = ["avi", "mp4", "mov"]
-		extension = filename.split('.')[-1]
-		return extension in valid_extensions
+class AsyncVideoLoader:
+	"""
+	This class can load videos using parallel processing and threads.
+	
+	Attributes:
+		source (string): Target video location.
+	"""
+	def __init__(self, source):
+		self.capture = cv2.VideoCapture(source)
 
-	def get_video_info(self, filename):
-		if not self.is_valid_video(filename):
-			return -1, -1
-		else:
-			stream = cv2.VideoCapture(filename)
-			frame_count = int(stream.get(cv2.CAP_PROP_FRAME_COUNT))
-			frame_rate = int(stream.get(cv2.CAP_PROP_FPS))
-			stream.release()
+		self.read_lock = threading.Lock()
+		self.started = False
 
-			return frame_rate, frame_count
+		self.success, self.frame = self.capture.read() 
 
-	def load_video(self, filename):
-		if self.is_valid_video(filename):
-			stream = cv2.VideoCapture(filename)
-			predictor = LandmarkPredictor()
-			frame_rate, current_frame = int(stream.get(cv2.CAP_PROP_FPS)), 0
-			means = []
+	# This method starts the class thread, unless it already started.
+	def start(self):
+		if self.started is True:
+			pass
 
-			while True:
-				success, frame = stream.read()
-				if not success:
-					break
+		self.thread = threading.Thread(target=self.update, args=())
+		self.started = True
+		self.thread.start()
 
-				face_rect = predictor.detect_face(image = frame)
-				if face_rect is not None:
-					face_bottom = face_rect.bottom()
-					face_right = face_rect.right()
-					face_left = face_rect.left()
-					face_top = face_rect.top()
+		return self
 
-					cut_face = frame[face_top:face_bottom, face_left:face_right, :]
+	def stop(self):
+		self.started = False
 
-					landmarks = predictor.detect_landmarks(image = cut_face)
-					roi = self.roi_extractor.extract_roi(cut_face, landmarks)
-					
-					if current_frame % 60 == 0 and current_frame > 0:
-						print("[VideoLoader] {0} frames extracted from '{1}'.".format(current_frame, filename))
-					
-					roi = roi.astype(np.float32)
-					roi[roi < 5.0] = np.nan
-
-					r = np.nanmean(roi[:, :, 2])
-					g = np.nanmean(roi[:, :, 1])
-					b = np.nanmean(roi[:, :, 0])
-
-					means += [[r, g, b]]
-
-					current_frame += 1
-				else:
-					print("[VideoLoader] Skipping '{0}'.\
-							Reason: Failure on Acquisition!\
-							(It's a folder??)".format(filename))
-					return None
-
-			return np.array(means)
-		else:
-			print("[VideoLoader] Skipping '{0}'. \
-					Reason: Invalid Video! (It's a folder??)".format(filename))
-			return None
-
-class FolderLoader:
-	def __init__(self, folder, file_list = None, time = 15, num_channels = 3, roi_extractor = CheeksAndNose()):
-		self.roi_extractor = roi_extractor
-		self.num_channels = num_channels
-		self.time = time
-
-		self.file_list = file_list
-		self.folder = folder
-
-		self.loader = VideoLoader(time = time, roi_extractor = roi_extractor)
-
-	def split_raw_data(self, raw_data, split_size, stride = 1):
-		data, i = None, 0
-		while i + split_size - 1 < len(raw_data):
-			if data is None:
-				data = np.array([raw_data[i : i + split_size, :]])
+	#This method update the current frame received from buffer.
+	def update(self):
+		while self.started:
+			success, frame = self.capture.read()
+			if not success:
+				self.stop()
 			else:
-				np.append(
-					data,
-					np.array([raw_data[i : i + split_size, :]]),
-					axis = 0
-				)
+				with self.read_lock:
+					self.success = success
+					self.frame = frame
 
-			i += stride
+	#This method return the current frame captured from buffer. (Thread safe?)
+	def read(self):
+		with self.read_lock:
+			frame = self.frame.copy()
+			success = self.success
 
-		return data
+		return success, frame
 
-	def load_data(self):
-		counter, data = 0, []
-		dir_size = len(os.listdir(self.folder))
-		for filename in sorted(os.listdir(self.folder)):
-			counter += 1
-			if (self.file_list is None) or ((self.file_list is not None) and (filename in self.file_list)):
-				print("[FolderLoader - {0} / {1}] Trying to load video '{2}'.".format(counter, dir_size, filename))
-				video_data = self.loader.load_video(filename = self.folder + '/' + filename)
-				if video_data is not None:
-					data += [video_data]
+	def lenght(self):
+		return int(self.capture.get(cv2.CAP_PROP_FRAME_COUNT))
 
-		return data
+	def __exit__(self, exec_type, exc_value, traceback):
+		self.capture.release()
 
-class SiW_Loader:
-	def __init__(self, data_folder):
-		self.data_folder = data_folder
 
-	def load_data(self, time):
-		def load_complex_folder(complex_folder, time = 20):
-			complex_folder_data = None
-			for folder in os.listdir(complex_folder):
-				data = FolderLoader(folder = "{0}/{1}".format(complex_folder, folder), time = time).load_data()
-				if complex_folder_data is None:
-					complex_folder_data = np.array(data)
+"""
+Class dedicated to load videos and crop from desired location \
+using the AsyncLoader defined previously and cropping the region of interest.
+"""
+class VideoLoader:
+	"""
+	Method name: load_features
+
+	This method represents the main method of this class. It loads a video \
+	based on source string and crop according to the information present in the
+	annotation files or dettect faces with the "dlib" library.
+
+
+	Attributes:
+		source (string): The source location of desired video.
+		annotation (string): The source location of desired video annotation.
+		border_notation (bool): Is related to the current data format of \
+								annotation file. \
+								If True, [x, y, u, v] describes (x, y) as \
+								the left supperior points and (u, v) the right \
+								inferior points.
+								If False, it means [x, y, width, height].
+	"""
+	@staticmethod
+	def load_features(source, annotation=None, border_notation=False):
+		print("[DataLoader] Extracting features from '{0}'.".format(source))
+
+		def load_annotations(annotation_location):
+			def is_digit(n):
+				try:
+					int(n)
+					return True
+				except ValueError:
+					return False
+
+			with open(annotation_location) as file:
+				annotations = []
+				for line in file.readlines():
+					line = line.replace(',', ' ')
+					annotation_line = list()
+					for i in line[:-1].split(' '):
+						if is_digit(i):
+							annotation_line.append(int(i))
+
+					if len(annotation_line) > 4:
+						annotation_line = annotation_line[-4:]
+
+					annotations += [annotation_line]
+
+			return np.array(annotations)
+
+		loader = AsyncVideoLoader(source=source).start()
+		predictor = LandmarkPredictor()
+		extractor = CheeksAndNose()
+		if annotation is not None:
+			annotations = load_annotations(annotation)
+		
+		features = np.zeros([loader.lenght(), 3], dtype=np.float32)
+		for i in range(loader.lenght()):
+			success, frame = loader.read()
+			def valid_annotation(annotations, current_index):
+				if len(annotations) < current_index:
+					return False
+				elif len(annotations[current_index]) != 4:
+					return False
 				else:
-					complex_folder_data = np.append(complex_folder_data, np.array(data), axis = 0)
+					for i in annotations[current_index]:
+						if i <= 0:
+							return False
+					return True
 
-			return complex_folder_data
+			if (annotation is not None) and not valid_annotation(annotations, i):
+				features[i] = 0 if (i == 0) else features[i - 1]
+			else:
+				if annotation is None:
+					rect = predictor.detect_face(frame)
+					if rect is None:
+						features[i] = 0 if (i == 0) else features[i - 1]
+					else:
+						left = rect.left()
+						right = rect.right()
+						top = rect.top()
+						bottom = rect.bottom()
 
-		train_spoof = load_complex_folder("{0}/Train/live".format(self.data_folder))
-		train_live = load_complex_folder("{0}/Train/live".format(self.data_folder))
+						frame = frame[top:bottom, left:right]
+				else:				
+					x, y, w, h = annotations[i]						
+					if not border_notation:
+						frame = frame[y:y+h, x:x+w]
+					else:
+						frame = frame[y:h, x:w]
 
-		test_spoof = load_complex_folder("{0}/Test/live".format(self.data_folder))
-		test_live = load_complex_folder("{0}/Test/live".format(self.data_folder))
+				landmarks = predictor.detect_landmarks(frame)
+				frame = extractor.extract_roi(frame, landmarks).astype(np.float32)
 
-		np.save("{0}/train_spoof.npy".format(self.data_folder), train_spoof)
-		np.save("{0}/train_live.npy".format(self.data_folder), train_live)
+				frame[frame == 0.0] = np.nan
+				features[i] = np.nanmean(frame, axis=(0, 1))
+				if np.any(np.isnan(features[i])):
+					features[i] = features[i - 1] if i > 0 else 0.0
 
-		np.save("{0}/test_spoof.npy".format(self.data_folder), test_spoof)
-		np.save("{0}/test_live.npy".format(self.data_folder), test_live)
+		loader.stop()
+		print("[DataLoader] Features extracted from '{0}'.".format(source))
+		return features
 
-class ReplayAttackLoader:
-	def __init__(self, data_folder):
-		self.data_folder = data_folder
+class GenericDatasetLoader:
+	@staticmethod
+	def walk_and_load_from(root_folder, annotations_folder=None, border_notation=False):
+		def is_video(filename):
+			return 	filename.endswith(".mp4")\
+					or filename.endswith(".mov")\
+					or filename.endswith(".avi")
 
-	def custom_shuffle(self, a, b):
-		assert len(a) == len(b)
-		p = np.random.permutation(len(a))
+		def is_annotation(filename):
+			return 	filename.endswith(".face")\
+					or filename.endswith(".txt")
+
+		video_locations = list()
+		for root, dirs, files in os.walk(root_folder):
+			for file in files:
+				if is_video(file):
+					video_locations.append("{0}/{1}".format(root, file))
+
+		if annotations_folder is not None:
+			annotations = list()
+			for root, dirs, files in os.walk(annotations_folder):
+				for file in files:
+					if is_annotation(file):
+						annotations.append("{0}/{1}".format(root, file))
+	
+			annotations = sorted(annotations)
+		else:
+			annotations = None
+
+		video_locations = sorted(video_locations)
+		folder_features = None
+		k = 0
+		for i in range(len(video_locations)):
+			video = video_locations[i]
+			annotation = annotations[i] if annotations is not None else None
+
+			video_data = VideoLoader.load_features(
+							source=video,
+							annotation=annotation,
+							border_notation=border_notation)
+
+			if folder_features is None:
+				folder_features = np.empty([len(video_locations),
+											video_data.shape[0],
+											3], dtype=np.float32)
+
+			else:
+				if video_data.shape[0] < folder_features.shape[1]:
+					new_video_data = np.zeros(folder_features.shape[1:])
+					new_video_data[:video_data.shape[0]] = video_data
+					video_data = new_video_data.copy()
+
+				elif video_data.shape[0] > folder_features.shape[1]:
+					video_data = video_data[:folder_features.shape[1]]
+
+			folder_features[k] = video_data			
+			k += 1
 		
-		return a[p], b[p]
+		return folder_features
 
-	def load_data(self, time):
-		validation = self.load_data_by_folder("{0}/devel".format(self.data_folder), time)
-		train = self.load_data_by_folder("{0}/train".format(self.data_folder), time)
-		test = self.load_data_by_folder("{0}/test".format(self.data_folder), time)
-
-		return [train, validation, test]
-
-	def load_data_by_folder(self, folder, time):
-		def replace_array_by_cuts(raw_data_bucket, data_loader, split_size):
-			new_data = None
-			for i in range(len(raw_data_bucket)):
-				split = data_loader.split_raw_data(raw_data_bucket[i], split_size, stride = 1)
-				new_data = split if new_data is None else np.append(new_data, split, axis = 0)
-			
-			return new_data
-
-		print("[ReplayAttackLoader] Loading ReplayAttack videos from '{0}/'.".format(folder))
-		fake_fixed_loader = FolderLoader(folder = folder + "/attack/fixed", time = time)
-		fake_hand_loader = FolderLoader(folder = folder + "/attack/hand", time = time)
-		real_loader = FolderLoader(folder = folder + "/real", time = time)
-
-		fake_fixed_data = fake_fixed_loader.load_data()
-		fake_hand_data = fake_hand_loader.load_data()
-		real = real_loader.load_data()
-
-		min_fixed = min([len(fake_fixed_data[i]) for i in range(len(fake_fixed_data))])
-		min_hand = min([len(fake_hand_data[i]) for i in range(len(fake_hand_data))])
-		min_real = min([len(real[i]) for i in range(len(real))])
-
-		cut_size = min(min_fixed, min_hand, min_real)
-
-		fake_hand_data = replace_array_by_cuts(np.array(fake_hand_data), fake_hand_loader, cut_size)
-		fake_fixed_data = replace_array_by_cuts(np.array(fake_fixed_data), fake_fixed_loader, cut_size)
-		real = replace_array_by_cuts(np.array(real), real_loader, cut_size)
-
-		fake = np.append(fake_hand_data, fake_fixed_data, axis = 0)
-		labels = np.append(np.zeros(fake.shape[0]), np.ones(real.shape[0]))
+def slice_and_stride(data, size, stride):	
+	def slice_and_stride_video(video):
+		slices = list()
+		for i in range(0, len(video), stride):
+			if i + size <= len(video):
+				slices.append(video[i:i+size])
 		
-		return [np.append(fake, real, axis = 0), to_categorical(labels, 2)]
+		return np.array(slices)
+
+	data_slice = None
+	for i in range(len(data)):
+		sl = slice_and_stride_video(data[i])
+		if data_slice is None:
+			data_slice = sl
+		else:
+			data_slice = np.append(data_slice, sl, axis = 0)
+
+	return data_slice
+
+	
