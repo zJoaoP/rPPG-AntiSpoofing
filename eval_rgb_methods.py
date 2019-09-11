01bedb66c75da201d9577bfd28fc33e21b1470bf
JoaoP <contato.joaocomp@gmail.com>
2019-09-11 17:37:23 -0300

    Adaptando códigos antigos ao modelo atual

--- eval_rgb_methods.py
+++ eval_rgb_methods.py
@@ -1,13 +1,16 @@
 from utils.landmarks import LandmarkPredictor
+from utils.extractor import CheeksAndNose
+import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 import cv2
 
 from utils.wrappers import OpenCV_Wrapper, Kinect_Wrapper
-from algorithms.de_haan_pos import DeHaanPOS
-from algorithms.fast_nir import FastNIR
 from algorithms.de_haan import DeHaan
 from algorithms.green import Green
+from algorithms.wang import Wang
+
+from utils.loaders import AsyncVideoLoader
 
 class MyArgumentParser:
 	def __init__(self):
@@ -23,38 +26,27 @@ class MyArgumentParser:
 							help = "Tempo (em segundos) de captura ou análise. (Padrão: %(default)s segundos)")
 		parser.add_argument("--use-kinect", dest = "use_kinect", action = "store_true", default = False,
 							help = "Define se o programa utilizará (ou não) um kinect para obter as imagens. (Padrão: %(default)s)")
-		parser.add_argument("--aproach", dest = "aproach", nargs = "?", default = ["Green", "DeHaan", "DeHaanPOS"], action = "store",
-							help = "Abordagem no cálculo do rPPG. (Padrão: %(default)s)")
 		return parser
-
-	def parse_strategies(self, aproaches):
-		strategies = []
-		for aproach in aproaches:
-			if aproach == "DeHaan":
-				strategies += [DeHaan()]
-			elif aproach == "DeHaanPOS":
-				strategies += [DeHaanPOS()]
-			elif aproach == "Green":
-				strategies += [Green()]
-
-		return strategies
 
 	def parse_args(self, args = None):
 		arguments = self.parser.parse_args(args)
-		strategies = self.parse_strategies(arguments.aproach)
 		wrapper = OpenCV_Wrapper(arguments.source) if (not arguments.use_kinect) or arguments.source != 0 else Kinect_Wrapper(width = 1280, height = 720)
 
-		return arguments, wrapper, strategies
+		return arguments, wrapper
 
 if __name__ == "__main__":
-	args, wrapper, strategies = MyArgumentParser().parse_args()
+	args, wrapper = MyArgumentParser().parse_args()
 	bgr_predictor = LandmarkPredictor()
 
-	frame_rate = wrapper.get_frame_rate()
 	frames_to_skip = 0
+	frame_rate = 30
+
+	loader = AsyncVideoLoader(args.source)
+	extractor = CheeksAndNose()
 
 	frame_count, frame_limit = 0, args.time * frame_rate
-	while frame_count < frame_limit:
+	temporal_means = None
+	while frame_count < loader.lenght():
 		(success, bgr, nir) = wrapper.get_frame()
 		if not success or cv2.waitKey(1) & 0xFF == ord('q'):
 			break
@@ -68,8 +60,19 @@ if __name__ == "__main__":
 				cut_bgr = bgr[bgr_top : bgr_bottom, bgr_left : bgr_right, :]
 
 				bgr_landmarks = bgr_predictor.detect_landmarks(image = cut_bgr)
-				for strategy in strategies:
-					strategy.process(cut_bgr, bgr_landmarks)
+				cut_bgr = extractor.extract_roi(cut_bgr, bgr_landmarks).astype(np.float32)
+				# for strategy in strategies:
+				# 	strategy.process(cut_bgr, bgr_landmarks)
+
+				cv2.imshow('WOW', cut_bgr.astype(np.uint8))
+
+				cut_bgr[cut_bgr == 0.0] = np.nan
+				r, g, b = np.nanmean(cut_bgr, axis=(0, 1))
+				features = np.array([[r, g, b]])
+				if temporal_means is None:
+					temporal_means = np.array([[r, g, b]])
+				else:
+					temporal_means = np.append(temporal_means, np.array([[r, g, b]]), axis=0)
 
 				# for x, y in bgr_landmarks:
 				# 	cv2.circle(cut_bgr, (x, y), 1, (0, 255, 0))
@@ -82,5 +85,14 @@ if __name__ == "__main__":
 			else:
 				frames_to_skip = args.skip_count	
 
-	for strategy in strategies:
-		strategy.show_results(frame_rate = frame_rate, window_size = int(frame_rate * 1.6), plot = True)
+	# print(temporal_means.shape)
+	signal = Wang.extract_rppg(temporal_means)
+	fig, (means, rppg) = plt.subplots(2)
+	means.plot(temporal_means)
+	rppg.plot(signal)
+
+	plt.show()
+
+	print(temporal_means.mean())
+	print(temporal_means.max())
+	print(temporal_means.min())
