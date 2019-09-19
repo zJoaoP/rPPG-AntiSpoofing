0d340407170019af21fd7af9908d1cf0d45de5e7
JoaoP <contato.joaocomp@gmail.com>
2019-09-19 18:28:44 -0300

    Pequenas alterações

--- train_rgb.py
+++ train_rgb.py
@@ -35,6 +35,15 @@ if __name__ == "__main__":
 	train_x, train_rppg_x, train_y = [None] * 3
 	devel_x, devel_rppg_x, devel_y = [None] * 3
 	test_x, test_rppg_x, test_y = [None] * 3
+
+	def shuffle(a, b, c):
+		rng_state = np.random.get_state()
+		np.random.shuffle(a)
+		np.random.set_state(rng_state)
+		np.random.shuffle(b)
+		np.random.set_state(rng_state)
+		np.random.shuffle(c)
+
 	if args.prefix == 'rad':
 		def load_partition(name, flip=False):
 			p_x = np.load("{0}/{1}_{2}_x.npy".format(args.source,
@@ -80,15 +89,6 @@ if __name__ == "__main__":
 		train_x = solve_nan_positions(train_x)
 		test_x = solve_nan_positions(test_x)
 
-		def shuffle(a, b, c):
-			rng_state = np.random.get_state()
-			np.random.shuffle(a)
-			np.random.set_state(rng_state)
-			np.random.shuffle(b)
-			np.random.set_state(rng_state)
-			np.random.shuffle(c)
-
-
 		shuffle(train_x, train_rppg_x, train_y)
 	elif args.prefix == 'oulu':
 		def load_split(split):
@@ -98,11 +98,14 @@ if __name__ == "__main__":
 
 			y = to_categorical(y, 2)
 
-			def normalize(data):
-				return (data - data.mean()) / data.std()
+			# def normalize(data):
+			# 	return (data - data.mean()) / data.std()
 
-			rppg = normalize(rppg)
-			x = normalize(x)
+			# rppg = normalize(rppg)
+			# x = normalize(x)
+			x = x / 255.0
+
+			shuffle(x, rppg, y)
 
 			return x, rppg, y
 
@@ -110,27 +113,41 @@ if __name__ == "__main__":
 		devel_x, devel_rppg_x, devel_y = load_split('devel')
 		test_x, test_rppg_x, test_y = load_split('test')
 
+		# train_x = norm(train_x)
+		# devel_x = norm(devel_x)
+		# test_x = norm(test_x)
+
+		# train_rppg_x = norm(train_rppg_x)
+		# devel_rppg_x = norm(devel_rppg_x)
+		# test_rppg_x = norm(test_rppg_x)
+
+	elif args.prefix == "3DMAD":
+		train_rppg_x = np.load("{0}/3DMAD_train_rppg_x.npy".format(args.source))
+		train_x = np.load("{0}/3DMAD_train_x.npy".format(args.source))
+		train_y = np.load("{0}/3DMAD_train_y.npy".format(args.source))
+
+		test_rppg_x = np.load("{0}/3DMAD_test_rppg_x.npy".format(args.source))
+		test_x = np.load("{0}/3DMAD_test_x.npy".format(args.source))
+		test_y = np.load("{0}/3DMAD_test_y.npy".format(args.source))
+
+		train_y = to_categorical(train_y, 2)
+		test_y = to_categorical(test_y, 2)
+
 	from model.structures import SimpleConvolutionalRPPG
 	from model.structures import SimpleConvolutionalRGB
 	from model.structures import DeepConvolutionalRPPG
 	from model.structures import DeepConvolutionalRGB
-	from model.structures import TripletRPPG
-	from model.structures import TripletRGB
-	from model.structures import FlatRPPG
-	from model.structures import FlatRGB
-	architectures = [TripletRGB,
-					 TripletRPPG,
-					 SimpleConvolutionalRGB,
+	from model.structures import SimpleResnetRGB
+	architectures = [SimpleConvolutionalRGB,
 					 SimpleConvolutionalRPPG,
 					 DeepConvolutionalRGB,
-					 DeepConvolutionalRPPG,
-					 FlatRGB,
-					 FlatRPPG]
+					 DeepConvolutionalRPPG]
 
 	batch_size = 16
-	verbose = False
-	epochs = 50000
+	verbose = True
+	epochs = 10
 
+	eer = list()
 	for arch in architectures:
 		arch_model = arch(dimension=train_x.shape[1], lr=1e-4, verbose=verbose)
 		arch_name = type(arch_model).__name__
@@ -138,27 +155,47 @@ if __name__ == "__main__":
 		model_dest = "./model/models/{0}.ckpt".format(arch_name)
 		model_ckpt = ModelCheckpoint(model_dest, monitor='val_ACER',
 												 save_best_only=True,
+												 mode='min',
 												 verbose=verbose)
-		early_stopping = EarlyStopping(monitor='val_ACER', patience=1000)
+
+		early_stopping = EarlyStopping(monitor='val_ACER', mode='min', patience=300)
 		callbacks = [model_ckpt, early_stopping]
 		if not arch_model.uses_rppg():
-			validation = None if train_x is None else (devel_x, devel_y)
+			validation = None if devel_x is None else (devel_x, devel_y)
 			arch_model.fit(x=train_x, y=train_y,
 									  epochs=epochs,
 									  batch_size=batch_size,
+									  # validation_split=0.2,
 									  validation_data=validation,
 									  callbacks=callbacks)
 
-			print(arch_model.evaluate(test_x, test_y))
+			arch_model.get_model().load_weights(model_dest)
+
+			evaluation = arch_model.evaluate(test_x, test_y)
+			print("{0} : {1}".format(arch_name, evaluation))
 		else:
-			validation = None if train_x is None else ([devel_x, devel_rppg_x], devel_y)
+			validation = None if devel_x is None else ([devel_x, devel_rppg_x], devel_y)
 			arch_model.fit(x=[train_x, train_rppg_x], y=train_y,
 													  epochs=epochs,
 													  batch_size=batch_size,
+													  # validation_split=0.2,
 													  validation_data=validation,
 													  callbacks=callbacks)
 
-			print(arch_model.evaluate([test_x, test_rppg_x], test_y))
+			arch_model.get_model().load_weights(model_dest)
 
-		model = arch_model.get_model()
-		plot_model(model, to_file='./model/models/images/{0}.png'.format(arch_name))
+			evaluation = arch_model.evaluate([test_x, test_rppg_x], test_y)
+			print("{0} : {1}".format(arch_name, evaluation))
+
+		# plot_model(model, to_file='./model/models/images/{0}.png'.format(arch_name))
+	# 	pred = pred[:, 1]
+	# 	from sklearn.metrics import roc_curve, auc, roc_auc_score
+	# 	import matplotlib.pyplot as plt
+	# 	fpr, tpr, threshold = roc_curve(np.argmax(train_y, axis=1), pred)
+
+	# 	fnr = 1 - tpr
+	# 	eer.append(fpr[np.nanargmin(np.absolute((fnr - fpr)))])
+	# 	print("EER = %f" % (fpr[np.nanargmin(np.absolute((fnr - fpr)))]))
+
+	# with open('3dmad_eer.txt', 'a+') as f:
+	# 	f.write("{0}\n".format(eer))
