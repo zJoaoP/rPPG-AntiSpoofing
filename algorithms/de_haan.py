b94c50ba9ae95f28a6fe5ce13342403aa139d980
JoaoP <contato.joaocomp@gmail.com>
2019-09-24 17:55:38 -0300

    Adaptando algoritmo ao novo padrão

--- algorithms/de_haan.py
+++ algorithms/de_haan.py
@@ -6,72 +6,41 @@ import numpy as np
 import cv2
 
 class DeHaan(DefaultStrategy):
-	def __init__(self, temporal_means = None):
-		self.temporal_means = temporal_means
-
-	def process(self, frame, landmarks):
-		frame = CheeksAndNose().extract_roi(frame, landmarks)
-		frame = np.array(frame, dtype = np.float32)
-
-		frame[frame == 0.0] = np.nan
-		r = np.nanmean(frame[:, :, 2])
-		g = np.nanmean(frame[:, :, 1])
-		b = np.nanmean(frame[:, :, 0])
-
-		if self.temporal_means is None:
-			self.temporal_means = np.array([[r, g, b]])
-		else:
-			self.temporal_means = np.append(self.temporal_means, [[r, g, b]], axis = 0)
-
-	def measure_reference(self, frame_rate = 30, window_size = 45):
+	@staticmethod
+	def extract_rppg(temporal_means, frame_rate=30):
 		def get_order(size):
 			return (size - 6) // 3
 
-		signal = np.zeros(len(self.temporal_means), dtype = np.float32)
-		window_stride = window_size // 2
+		signal = np.zeros(len(temporal_means), dtype=np.float32)
+
+		window_size = int(frame_rate * 1.6)
 
 		for i in range(3):
-			self.temporal_means[:, i] = self.temporal_means[:, i] - np.mean(self.temporal_means[:, i])
-			self.temporal_means[:, i] = self.detrend(self.temporal_means[:, i])
+			temporal_means[:, i] = (temporal_means[:, i] / np.mean(temporal_means[:, i])) - 1.0
+			temporal_means[:, i] = DefaultStrategy.detrend(temporal_means[:, i])
+			temporal_means[:, i] = DefaultStrategy.bandpass_filter(temporal_means[:, i],
+																   frame_rate=frame_rate,
+																   min_freq=0.6,
+																   max_freq=4.0,
+																   order=get_order(window_size))
 
-		for j in range(0, len(self.temporal_means) - window_size, window_stride):
-			if j + window_size > len(self.temporal_means):
-				j = len(self.temporal_means) - window_size
+			# self.temporal_means[:, i] = self.detrend(self.temporal_means[:, i])
 
-			r, g, b = [self.temporal_means[j : j + window_size, i] for i in range(3)]
+		for j in range(0, len(temporal_means), window_size // 2):
+			if j + window_size > len(temporal_means):
+				break
+
+			r, g, b = [temporal_means[j : j + window_size, i] for i in range(3)]
 
 			Xs = (3.0 * r) - (2.0 * g)
 			Ys = (1.5 * r) + (1.0 * g) - (1.5 * b)
 
-			Xf = self.bandpass_filter(Xs, frame_rate = frame_rate, min_freq = 0.7, max_freq = 4.0, order = get_order(window_size))
-			Yf = self.bandpass_filter(Ys, frame_rate = frame_rate, min_freq = 0.7, max_freq = 4.0, order = get_order(window_size))
+			Xf = DefaultStrategy.bandpass_filter(Xs, frame_rate=frame_rate, min_freq=0.6, max_freq=4.0, order=get_order(window_size))
+			Yf = DefaultStrategy.bandpass_filter(Ys, frame_rate=frame_rate, min_freq=0.6, max_freq=4.0, order=get_order(window_size))
 
 			alpha = np.std(Xf) / np.std(Yf)
 
-			window_signal = (Xf - (alpha * Yf)) * np.hanning(window_size)
-			signal[j : j + window_size] += window_signal
+			signal[j:j + window_size] += np.hanning(window_size) * (Xf - (alpha * Yf))
 
 		return (signal - np.mean(signal)) / np.std(signal)
-
-	def show_results(self, frame_rate = 30, window_size = 60, plot = True):
-		signal = self.measure_reference(frame_rate = frame_rate, window_size = window_size)
-		x, y = self.get_fft(signal, frame_rate = frame_rate)
-
-		if plot == True:
-
-			plt.subplot(3, 1, 1)
-			
-			plt.title("DeHaan")
-			plt.plot(self.temporal_means[:, 0], 'r')
-			plt.plot(self.temporal_means[:, 1], 'g')
-			plt.plot(self.temporal_means[:, 2], 'b')
-
-			plt.subplot(3, 1, 2)
-			plt.plot(signal)
-
-			plt.subplot(3, 1, 3)
-			plt.plot(x, y)
-	
-			plt.show()
-
-		print("[DeHaan] {0:2f} bpm.".format(60.0 * x[np.argmax(y)]))
+		# return signal
